68397e122d29c7b1757d3dedb7efac751823e90e
Shubham.Apshete <shubham.apshete@brillio.com>
2021-06-18 10:58:32 +0530

    commit newsapp working

--- favoriteservice/src/main/java/com/tackroute/favoriteservice/model/News.java
+++ favoriteservice/src/main/java/com/tackroute/favoriteservice/model/News.java
@@ -2,22 +2,32 @@ package com.tackroute.favoriteservice.model;
 
 public class News {
 
-        private String favoriteId;
-        private String newsTitle;
-        private String source;
-        private String author;
-        private String title;
-        private String description;
+    private String favoriteId;
+    private String newsTitle;
+    private String source;
+    private String author;
+    private String title;
+    private String description;
+    private String url;
+    private String urlToImage;
+    private String publishedAt;
+    private String content;
 
-   public News(){}
+    public News() {
+    }
 
-    public News(String favoriteId, String newsTitle, String source, String author, String title, String description) {
+    public News(String favoriteId, String newsTitle, String source, String author, String title, String description, String url, String urlToImage, String publishedAt, String content) {
+
         this.favoriteId = favoriteId;
         this.newsTitle = newsTitle;
         this.source = source;
         this.author = author;
         this.title = title;
         this.description = description;
+        this.url = url;
+        this.urlToImage = urlToImage;
+        this.publishedAt = publishedAt;
+        this.content = content;
     }
 
     public String getFavoriteId() {
@@ -68,7 +78,37 @@ public class News {
         this.description = description;
     }
 
+    public String getUrl() {
+        return url;
+    }
 
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUrlToImage() {
+        return urlToImage;
+    }
+
+    public void setUrlToImage(String urlToImage) {
+        this.urlToImage = urlToImage;
+    }
+
+    public String getPublishedAt() {
+        return publishedAt;
+    }
+
+    public void setPublishedAt(String publishedAt) {
+        this.publishedAt = publishedAt;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
 
 
