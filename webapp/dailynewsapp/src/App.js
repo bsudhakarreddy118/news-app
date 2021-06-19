2a191d43cfa0067c6e7d27fb4de96be8d3916445
REKADI.SIVAKUMAR <rekadi.sivakumar@brillio.com>
2021-06-19 23:24:52 +0530

    commit Register

--- webapp/dailynewsapp/src/App.js
+++ webapp/dailynewsapp/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Header from './Components/header/Header';
 import Footer from './Components/footer/Footer';
 // import EditProfile from './Components/EditProfile/EditProfile';
-import Login from './Components/Login/Login';
+// import Login from './Components/Login/Login';
 
 // import LandingPage from './Components/LandingPage/LandingPage';
 import Registration from './Components/Registeration/Registeration';
@@ -12,13 +12,13 @@ function App() {
     <div className="App">
       {/* <Header/> */}
 
-      <Login/>
+      {/* <Login/> */}
 {/* 
       <EditProfile/> */}
 
       {/* <LandingPage/> */}
-{/* 
-      <Registration/> */}
+
+      <Registration/>
 
       <Footer/>
     </div>
