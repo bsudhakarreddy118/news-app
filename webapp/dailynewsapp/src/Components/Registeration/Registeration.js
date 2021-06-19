29c51c940b708b1ee6c4b363d0a63e931f3a6dc3
REKADI.SIVAKUMAR <rekadi.sivakumar@brillio.com>
2021-06-19 23:03:06 +0530

    commit Login

--- webapp/dailynewsapp/src/Components/Registeration/Registeration.js
+++ webapp/dailynewsapp/src/Components/Registeration/Registeration.js
@@ -1,33 +1,104 @@
 import React from 'react';
-import "./Registration.css";
-
+ import "./Registration.css";
+import { useState } from "react";
 export default function Registration() {
 
-    // const [username, setUsername] = useState('');
-    // const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    // function Registration() {
-    //     fetch('http://localhost:3001/auth/v1', {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         },
-    //         body: JSON.stringify({ username, password })
-    //     }).then(res => res.json())
-    //         .then(data => localStorage.setItem('token', data.token));
+
+function handler(){
+  
+fetch('http://localhost:8084/user-service/api/v1/register', {      
+        method: 'POST',            
+  headers: {                
+    'Content-Type': 'application/json'          
+    },           
+   body: JSON.stringify({ username,email,password })       
+ }).then(res =>{            
+  if(res.status==200){               
+     alert("Registered Succesfully")          
+  
+    }            else{             
+       alert("Unsuceessfull")          
+    }        });
+
+
+
+
+}
+
+
+
+
+
+
+
+
+    // return (
+    //     <div className="container my-5">
+    //       <h3 className="text-primary text-center">Registration</h3>
+    //       <div className="form-group">
+            
+    //         <input
+    //         onChange={(e)=>setUsername(e.target.value)}
+    //           type="text"
+    //           className="form-control"
+    //           id="exampleInputUsername"
+    //           aria-describedby="userNameHelp"
+    //           placeholder="Enter username"/>
+    //       </div>
+    //       <div className="form-group">
+            
+    //         <input
+    //          onChange={(e)=>setEmail(e.target.value)}
+    //           type="email"
+    //           className="form-control"
+    //           id="exampleInputEmail1"
+    //           placeholder="Email"
+    //         />
+    //       </div>
+    //       <div className="form-group">
+            
+    //         <input
+    //          onChange={(e)=>setPassword(e.target.value)}
+    //           type="password"
+    //           className="form-control"
+    //           id="exampleInputPassword1"
+    //           placeholder="Password"
+    //         />
+    //       </div>
+    
+    //        <button type="submit" className="btn btn-primary" onClick={handler}>
+    //         Submit
+    //       </button>
+    //     </div>
+    //   );
     // }
+    
+
+
+    
     return (
 
-    <div>
-         
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                    <div className="container-fluid">
-                    <a className="navbar-brand" href="#">NewsApp</a>
-                    <div className="text-right">
-                    <button className="btn btn-outline-success" >Login</button>
-                    </div>
-                    </div>
-            </nav>
+      <div>
+
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+
+                  <div className="container-fluid">
+
+                  <a className="navbar-brand" href="#">NewsApp</a>
+
+                  <div className="text-right">
+
+                  {/* <button className="btn btn-outline-success" ></button> */}
+
+                  </div>
+
+                  </div>
+
+          </nav>
 
          <div className="container">
 
@@ -37,22 +108,22 @@ export default function Registration() {
                     <form className="signin-form">
                             <div className="form-group">
                                  <i className="fas fa-user"></i>
-                                 <input type="text"  className="form-control"  placeholder="Username"/>
+                                 <input type="text"  onChange={(e)=>setUsername(e.target.value)} className="form-control"  placeholder="Username"/>
                             </div>
                             <div className="form-group">
                                 <i className="fas fa-key"></i>
-                                <input type="email"  className="form-control" placeholder="email"/>
+                                <input type="email" onChange={(e)=>setEmail(e.target.value)} className="form-control" placeholder="email"/>
                             </div>
                             <div className="form-group">
                                 <i className="fas fa-key"></i>
-                                <input type="password"  className="form-control" placeholder="Password"/>
+                                <input type="password"  onChange={(e)=>setPassword(e.target.value)}  className="form-control" placeholder="Password"/>
                             </div>
                             <div className="text-center"  >
-                                <button className="btn btn-success ">Register</button>
+                                <button className="btn btn-success" onClick={handler}>Register</button>
                          </div>
                     </form>
              </div>
         </div>
     </div>    
     )
-}
+    }
